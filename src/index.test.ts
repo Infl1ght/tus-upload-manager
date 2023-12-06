eaa8c674a83bc59b71724798f440e8afb24841a2
Ilya Volkov <ilya.inflight@gmail.com>
2023-12-06 10:53:51 +0400

    first commit

---
+++ src/index.test.ts
@@ -0,0 +1,205 @@
+import UploadManager, { IUploadCallbacks, IUploadTask, IUploader, IUploaderConstructor } from '.'
+
+const TIME_TO_TASK = 30
+const TIME_DELTA = 30
+
+const createUploader: IUploaderConstructor = function (
+  file: any,
+  options: {
+    metadata?: any
+    onError?: ((error: Error) => void) | null | undefined
+    onProgress?: ((bytesSent: number, bytesTotal: number) => void) | null | undefined
+    onSuccess?: (() => void) | null | undefined
+  }
+): IUploader {
+  let aborted = false
+  return {
+    start: jest.fn().mockImplementation(() => {
+      setTimeout(() => {
+        if (options.onSuccess && !aborted) {
+          options.onSuccess()
+        }
+      }, TIME_TO_TASK)
+    }),
+    abort: jest.fn().mockImplementation(() => {
+      return new Promise<void>(res =>
+        setTimeout(() => {
+          aborted = true
+          res()
+        }, 20)
+      )
+    }),
+  }
+}
+
+const createWithErrorUploader: IUploaderConstructor = function (
+  file: any,
+  options: {
+    metadata?: any
+    onError?: ((error: Error) => void) | null | undefined
+    onProgress?: ((bytesSent: number, bytesTotal: number) => void) | null | undefined
+    onSuccess?: (() => void) | null | undefined
+  }
+): IUploader {
+  const mockErrorTasks = 1
+  return {
+    start: jest.fn().mockImplementation(() => {
+      setTimeout(() => {
+        if (options.metadata.photoId < mockErrorTasks) {
+          if (options.onError) {
+            options.onError(new Error())
+          }
+        } else {
+          if (options.onSuccess) {
+            options.onSuccess()
+          }
+        }
+      }, TIME_TO_TASK)
+    }),
+    abort: () => {
+      return Promise.resolve()
+    },
+  }
+}
+
+const onSuccessMock = jest.fn()
+const onErrorMock = jest.fn()
+const onStartMock = jest.fn()
+const onHoldMock = jest.fn()
+
+const getTasks = (flightId: number, numberOfTasks: number, startId = 0): IUploadTask[] => {
+  const result = []
+  for (let i = 0; i < numberOfTasks; i++) {
+    result.push({
+      file: new File([new ArrayBuffer(1000)], 'test'),
+      id: i + startId,
+      queueId: flightId,
+      meta: undefined,
+    })
+  }
+  return result
+}
+
+describe('Upload Manager', () => {
+  let uploadManager: UploadManager
+  const callbacks: IUploadCallbacks = {
+    onSuccess: onSuccessMock,
+    onError: onErrorMock,
+    onProgress: jest.fn(),
+    onStart: onStartMock,
+    onHold: onHoldMock,
+  }
+
+  beforeEach(() => {
+    onSuccessMock.mockClear()
+    onErrorMock.mockClear()
+    onStartMock.mockClear()
+    onHoldMock.mockClear()
+
+    uploadManager = new UploadManager(
+      createUploader,
+      callbacks,
+      { uploadRetryDelays: [3000, 6000, 9000, 12000, 15000, 18000, 21000, 30000, 60000], maxUploadThreads: 3, onOfflineMessage: 'Offline' }
+    )
+  })
+  it('Smoke test', () => {
+    expect(uploadManager).toBeDefined()
+  })
+
+  it('Start 2 tasks', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 2)
+
+    uploadManager.addUploadTasks(flightId, tasks)
+
+    const holdEvent = onHoldMock.mock.calls
+    expect(holdEvent[0][0].length).toBe(2)
+
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(2)
+  })
+
+  it('Start 2 tasks, then add 2 tasks', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 2)
+    const tasks_next = getTasks(flightId, 2, 2)
+
+    uploadManager.addUploadTasks(flightId, tasks)
+    uploadManager.addUploadTasks(flightId, tasks_next)
+    await new Promise<void>(res => setTimeout(res, 2 * TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(4)
+  })
+
+  it('Start 2 tasks, then add 2 tasks after a while', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 2)
+
+    uploadManager.addUploadTasks(flightId, tasks)
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+    uploadManager.addUploadTasks(flightId, tasks)
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(4)
+  })
+
+  it('Start 2 tasks, 1 with error', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 2)
+    uploadManager = new UploadManager(createWithErrorUploader, callbacks, { uploadRetryDelays: [3000, 6000, 9000, 12000, 15000, 18000, 21000, 30000, 60000], maxUploadThreads: 3, onOfflineMessage: 'Offline' })
+
+    uploadManager.addUploadTasks(flightId, tasks)
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(1)
+
+    expect(onErrorMock.mock.calls.length).toBe(1)
+  })
+
+  it('Start 3 tasks, delete 1 holding task', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 3)
+
+    uploadManager.addUploadTasks(flightId, tasks)
+
+    await uploadManager.deleteUploadTask(flightId, 1)
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(2)
+  })
+
+  it('Start 2 tasks, delete 1 executing task', async () => {
+    const flightId = 1
+    const tasks = getTasks(flightId, 2)
+
+    uploadManager.addUploadTasks(flightId, tasks)
+
+    await uploadManager.deleteUploadTask(flightId, 0)
+    await new Promise<void>(res => setTimeout(res, TIME_TO_TASK * tasks.length + TIME_DELTA))
+
+    expect(onSuccessMock.mock.calls.length).toBe(1)
+  })
+
+  it('Start 4 queues, 1 queue must be on hold', async () => {
+    const flightIds = [0, 1, 2, 3]
+    const tasksQueue = flightIds.map(flightId => getTasks(flightId, 1))
+    tasksQueue.forEach((tasks, index) => uploadManager.addUploadTasks(flightIds[index], tasks))
+
+    expect(onStartMock.mock.calls.length).toBe(3)
+    expect(onHoldMock.mock.calls.length).toBe(4)
+  })
+
+  it('Start queue, then abort it', async () => {
+    const flightIds = [0]
+    const tasksQueue = flightIds.map(flightId => getTasks(flightId, 1))
+    tasksQueue.forEach((tasks, index) => uploadManager.addUploadTasks(flightIds[index], tasks))
+
+    uploadManager.stopUploadingForQueue(0)
+
+    expect(onStartMock.mock.calls.length).toBe(1)
+    expect(onHoldMock.mock.calls.length).toBe(1)
+    expect(onErrorMock.mock.calls.length).toBe(0)
+    expect(onSuccessMock.mock.calls.length).toBe(0)
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,234 @@
+import PQueue from 'p-queue'
+import { UploadOptions } from 'tus-js-client'
+
+export interface IUploadTask {
+  file: File
+  id: number
+  queueId: number
+  tusdId?: string
+  metadata?: any
+}
+
+export interface IUploaderConstructor {
+  (file: File | Blob, options: UploadOptions): IUploader
+}
+
+export interface IUploader {
+  start(): void
+  abort(shouldTerminate?: boolean): Promise<void>
+}
+
+export interface IUploadCallbacks {
+  onSuccess(taskId: number): void
+  onError(taskId: number): void
+  onProgress(taskId: number, bytesUploaded: number, bytesTotal: number, percentage: number): void
+  onStart(taskId: number): void
+  onHold(taskId: number[]): void
+}
+
+export class UploadQueue {
+  callbacks: IUploadCallbacks
+  queueId: number
+
+  abortControllers: { [id: number]: AbortController } = {}
+  queue: PQueue = new PQueue({ concurrency: 1 })
+  currentTaskId: number | undefined = undefined
+  createUploader: IUploaderConstructor
+
+  constructor(callbacks: IUploadCallbacks, createUploader: IUploaderConstructor, queueId: number) {
+    this.callbacks = callbacks
+    this.queueId = queueId
+    this.createUploader = createUploader
+  }
+
+  addNewTask(task: IUploadTask, uploadRetryDelays: number[]) {
+    this.queue.add(
+      ({ signal }) => {
+        try {
+          if (!this.abortControllers[task.id]) {
+            return true
+          }
+          this.currentTaskId = task.id
+          return new Promise((resolve, reject) => {
+            const uploader = this.createUploader(task.file, {
+              endpoint: '/uploads/',
+              uploadUrl: task?.tusdId ? `/uploads/${task?.tusdId}` : undefined,
+              retryDelays: uploadRetryDelays,
+              metadata: task.metadata,
+              onError: error => {
+                this.callbacks.onError(task.id)
+                this.currentTaskId = undefined
+                delete this.abortControllers[task.id]
+                resolve(error)
+              },
+              onShouldRetry: (error: any, retryAttempt: number, options: UploadOptions) => {
+                var status = error.originalResponse ? error.originalResponse.getStatus() : 0
+                if (status === 403) {
+                  return false
+                }
+                return true
+              },
+              onProgress: async (bytesUploaded, bytesTotal) => {
+                const percentage = parseFloat(((bytesUploaded / bytesTotal) * 100).toFixed(2))
+                this.callbacks.onProgress(task.id, bytesUploaded, bytesTotal, percentage)
+              },
+              onSuccess: () => {
+                this.callbacks.onSuccess(task.id)
+                this.currentTaskId = undefined
+                delete this.abortControllers[task.id]
+                resolve(true)
+              },
+            })
+            this.callbacks.onStart(task.id)
+            uploader.start()
+
+            signal?.addEventListener('abort', async () => {
+              try {
+                delete this.abortControllers[task.id]
+                await uploader.abort()
+                signal?.dispatchEvent(new Event('abort_complete'))
+                resolve(true)
+              } catch (error) {
+                console.error(error)
+              }
+            })
+          })
+        } catch (error) {
+          this.callbacks.onError(task.id)
+        }
+      },
+      { signal: this.abortControllers[task.id].signal }
+    )
+  }
+
+  async addTasks(tasks: IUploadTask[], uploadRetryDelays: number[]) {
+    tasks.forEach(async task => {
+      this.abortControllers[task.id] = new AbortController()
+    })
+    tasks.forEach(task => {
+      this.addNewTask(task, uploadRetryDelays)
+    })
+  }
+
+  deleteUploadTask(taskId: number) {
+    return new Promise<void>(res => {
+      if (this.currentTaskId !== taskId) {
+        delete this.abortControllers[taskId]
+        res()
+      } else {
+        const onAborted = () => res()
+        this.abortControllers[taskId].signal.addEventListener('abort_complete', onAborted)
+        this.abortControllers[taskId]?.abort()
+      }
+    })
+  }
+
+  start(tasks: IUploadTask[], uploadRetryDelays: number[]) {
+    return new Promise<number>((res, rej) => {
+      this.addTasks(tasks, uploadRetryDelays)
+      this.queue.on('idle', () => {
+        this.currentTaskId = undefined
+        res(this.queueId)
+      })
+      this.queue.on('error', e => {
+        console.error('Error with task queue ' + this.queueId)
+        console.error(e)
+      })
+    })
+  }
+
+  abort() {
+    return new Promise<void>(res => {
+      this.queue.clear()
+      if (this.currentTaskId && this.abortControllers[this.currentTaskId]) {
+        const onAborted = () => res()
+        this.abortControllers[this.currentTaskId].signal.addEventListener('abort_complete', onAborted)
+        this.abortControllers[this.currentTaskId]?.abort()
+      } else {
+        res()
+      }
+    })
+  }
+}
+
+export interface IUploadManagerOptions {
+  maxUploadThreads: number
+  uploadRetryDelays: number[]
+  onOfflineMessage: string
+}
+
+class UploadManager {
+  uploadQueues: { [queueId: string]: UploadQueue } = {}
+  queue: PQueue = new PQueue()
+  createUploader: IUploaderConstructor
+  callbacks: IUploadCallbacks | undefined = undefined
+  maxUploadThreads: number
+  uploadRetryDelays: number[]
+  onOfflineMessage: string
+
+  constructor(createUploader: IUploaderConstructor, callbacks: IUploadCallbacks, options: IUploadManagerOptions) {
+    this.createUploader = createUploader
+    this.callbacks = callbacks
+    this.maxUploadThreads = options.maxUploadThreads
+    this.uploadRetryDelays = options.uploadRetryDelays
+    this.onOfflineMessage = options.onOfflineMessage
+    this.queue = new PQueue({ concurrency: this.maxUploadThreads })
+
+    this.queue.on('completed', queueId => {
+      delete this.uploadQueues[queueId]
+    })
+    this.queue.on('error', error => console.error('Some error with queue: ' + error))
+
+    window.addEventListener('beforeunload', event => {
+      if (this.queue.pending > 0) {
+        event.preventDefault()
+        event.returnValue = ''
+      } else {
+        delete event['returnValue']
+      }
+    })
+
+    window.addEventListener('offline', e => {
+      if (this.queue.pending > 0) {
+        alert(this.onOfflineMessage)
+      }
+    })
+  }
+
+  addUploadTasks(queueId: number, tasks: IUploadTask[]) {
+    if (this.callbacks === undefined) {
+      throw new Error('UploadManager is not inited')
+    }
+    if (!tasks.length) {
+      return
+    }
+    const callbacks: IUploadCallbacks = this.callbacks
+    callbacks.onHold(tasks.map(item => item.id))
+    if (!this.uploadQueues[queueId]) {
+      this.queue.add(() => {
+        this.uploadQueues[queueId] = new UploadQueue(callbacks, this.createUploader, queueId)
+        return this.uploadQueues[queueId].start(tasks, this.uploadRetryDelays)
+      })
+    } else {
+      this.uploadQueues[queueId].addTasks(tasks, this.uploadRetryDelays)
+    }
+  }
+
+  async deleteUploadTask(queueId: number, taskId: number) {
+    const uploadQueue = this.uploadQueues[queueId]
+    if (!uploadQueue) {
+      return
+    }
+    await uploadQueue.deleteUploadTask(taskId)
+  }
+
+  async stopUploadingForQueue(queueId: number) {
+    const uploadQueue = this.uploadQueues[queueId]
+    if (!uploadQueue) {
+      return
+    }
+    await uploadQueue.abort()
+  }
+}
+
+export default UploadManager
